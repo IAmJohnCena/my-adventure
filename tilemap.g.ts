e1d48d58d0f126f4cd591d9e93d3ae69fb101a1d
IAmJohnCena <107245805+IAmJohnCena@users.noreply.github.com>
2022-06-10 09:54:11 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001020202020303030303030303030304080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c100c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0d0a0c0c0c0c0c0c09080c0c0c0c0c0c0b0e0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c09080c0c0c0c0c0c0c0c0c0c0c0f0c0c09080c0c0c0c0c0c0c0c0c0c0c0c0c0c0905070707070707070707070707070706`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . 2 2 . . . . . . 2 
+2 . . . . . . 2 2 . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.floorLight4,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.stairLarge,sprites.dungeon.chestClosed], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
